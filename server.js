afde26d6ea2b7629cfc18f6deab6bc2b23faeb71
Damjan Popović <damjan.popovic@fer.unizg.hr>
2025-01-29 18:40:22 +0100

    Update server.js

--- server.js
+++ server.js
@@ -22,6 +22,9 @@ const pool = new Pool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: 5432,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
 
 app.post("/api/data", async (req, res) => {
