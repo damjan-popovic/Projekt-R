02ce02cb7e13c78c41906542bb6f90c12db745ef
Damjan Popović <damjan.popovic@fer.unizg.hr>
2025-01-29 17:56:49 +0100

    Update map.js

--- client/static/map.js
+++ client/static/map.js
@@ -138,7 +138,7 @@ document.addEventListener("DOMContentLoaded", function () {
         selectedCounty = document.getElementById("selectedCounty").value || 'nijedno';
         selectedSubregion = document.getElementById("selectedSubregion").value || 'nijedno';
 
-        fetch('http://localhost:3000/api/data', {
+        fetch('https://isohr.onrender.com/api/data', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
